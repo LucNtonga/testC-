4eb7204acaf0aaf617346cac5665186e4f056268
LucNtonga <luc@LUCNTONGA87BB>
2017-06-13 01:57:46 +0200

    wkip

---
+++ Payroll/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll
+{
+	class Employee
+	{
+		private string name;
+		private DateTime birthday;
+		private decimal hourlySalary;
+		private string jobTitle;
+		private decimal taxRate;
+
+		public Employee(string name, DateTime birthday, decimal hourlySalary, string jobTitle)
+		{
+			this.name = name;
+			this.birthday = birthday;
+			this.hourlySalary = hourlySalary;
+			this.jobTitle = jobTitle;
+			this.taxRate = 0.25m;
+		}
+
+		public override string ToString()
+		{
+			return string.Format("Name:{0}, Birthday:{1:d}, Hourly Salary: {2:c}, Job Title:{3}", name, birthday, hourlySalary, jobTitle);
+		}
+	}
+}
---
+++ Payroll/EmployeeForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Payroll
+{
+	public partial class EmployeeForm : Form
+	{
+		public EmployeeForm()
+		{
+			InitializeComponent();
+		}
+
+		private void EmployeeForm_Load(object sender, EventArgs e)
+		{
+			DateTime birthday = new DateTime(1999,5,1);
+			Employee spongebob = new Employee("SpongeBob", birthday, 10.5m, "Burger Flipper");
+			lblEmployee.Text = spongebob.ToString();
+
+		}
+	}
+}
